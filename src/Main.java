0c0b0d920c742305604b1bf5bef495d8523c51b0
Ilya27m <126259888+Ilya27m@users.noreply.github.com>
2023-06-26 11:59:04 +0300

    afdsbg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        MyLinkedList l = new MyLinkedList();
+        l.add(7);
+        l.add(9);
+        l.add(6);
+        l.add(1);
+        l.add(8);
+        l.display();
+        l.removeO(8);
+        l.display();
 
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,57 +1,67 @@
 public class MyLinkedList<E> {
-    private  class Node {
+    private class Node {
         public E element;
-        public  Node next;
+        public Node next;
+
         public Node(E element) {
             this.element = element;
             next = null;
         }
+
         public Node(E element, Node next) {
             this.element = element;
-            this.next =next;
+            this.next = next;
         }
+
         public String toString() {
-            return  "Node = " + this.element.toString();
+            return "Node = " + this.element.toString();
         }
     }
-    private  int size;
+
+    private int size;
     private Node head;
+
     public MyLinkedList() {
         size = 0;
         head = null;
     }
+
     private Node getNode(int index) {
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
         Node node = head;
         for (int i = 0; i < index; i++) {
             node = node.next;
         }
-        return  node;
+        return node;
     }
+
     public E get(int index) {
         Node node = getNode(index);
         return node.element;
     }
+
     public int indexOf(Object obj) {
         Node node = head;
         for (int i = 0; i < size; i++) {
-            if (obj.equals(node.element)) return  i;
+            if (obj.equals(node.element)) return i;
             node = node.next;
         }
         return -1;
     }
+
     public boolean add(E element) {
         if (size == 0) {
             head = new Node(element);
-            return true;
         } else {
             Node node = head;
-            for (; node.next != null; node = node.next) {}
+            for (; node.next != null; node = node.next) {
+            }
             node.next = new Node(element);
             size++;
-            return true;
         }
+        return true;
     }
+
     public void add(int index, E element) {
         if (index < 0 || index > size) throw new IndexOutOfBoundsException();
         if (index == 0 ) {
@@ -62,7 +72,7 @@ public class MyLinkedList<E> {
         node.next = new Node(element, node.next);
         }
     }
-    public  E remove(int index) {
+    public  E removeI(int index) {
         if (index < 0 || index > size) throw new IndexOutOfBoundsException();
         Node node3 = null;
         if (size == 1) {
@@ -83,12 +93,20 @@ public class MyLinkedList<E> {
         size--;
         return node3.element;
     }
-    public  boolean remove(Object obj) {
+    public  boolean removeO(Object obj) {
         int index = indexOf(obj);
         if (index != -1) {
-            remove(index);
+            removeI(index);
             return true;
         }
         else return false;
     }
+    public void display() {
+        Node node = head;
+        while (node != null) {
+            System.out.println(node.element + " ");
+            node = node.next;
+        }
+    }
 }
+
